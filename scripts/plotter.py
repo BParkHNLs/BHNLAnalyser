e6bb3f884a3dc6dce59703f7cf5b9f7f3389c9a6
anlyon <alyon@phys.ethz.ch>
2021-10-01 17:41:33 +0200

    yields_checks.py added

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -444,187 +444,6 @@ class Plotter(Tools):
     canv.SaveAs('{}/{}.pdf'.format(outputdir, self.quantity.label))
 
 
-  def plotYields(self, lumi=0.774, selection='', title='', outdirlabel=''):
-
-    canv = self.tools.createTCanvas('canv', dimx=900, dimy=800)
-    canv.SetLogx()
-    canv.SetLogy()
-    canv.SetGrid()
-
-
-    # gen yields
-    gen_coupling_m1 = [5.37675839253e-06, 5.37675839253e-05, 0.000537675839253, 0.00537675839253, 0.0537675839253]
-    #gen_yields_m1 = [0.233520553543, 21.1107914574, 2521.79127222, 198534.792249, 9787694.93131] # wrong weight
-    #gen_yields_m1 = [0.222669464601, 21.7748154625, 2054.4123073, 164459.789617, 5551279.31665] # good weight, without trg mu eff
-    gen_yields_m1 = [0.271587023481, 26.2785112766, 2480.63915671, 186917.31518, 6378867.68389] # good weight, with trg mu eff
-
-    gen_coupling_m3 = [2.2126577747e-06, 2.2126577747e-05, 0.00022126577747, 0.0022126577747]
-    #gen_yields_m3 = [0.0149191801294, 1.05017745728, 41.0813274363, 692.810973848]
-    #gen_yields_m3 = [0.0199334286553, 1.14028241247, 23.582224495, 228.202498809]
-    gen_yields_m3 = [0.0238935820319, 1.63795916047, 37.1400371471, 368.634025203]
-
-    gen_coupling_m4p5 = [2.91378801607e-06, 2.91378801607e-05, 0.000291378801607, 0.00291378801607] 
-    #gen_yields_m4p5 = [0.000314442951588, 0.0164840965898, 0.376472439332, 4.08626422153]
-    #gen_yields_m4p5 = [0.000446445913129, 0.0127053455282, 0.160543306967, 1.68758530476]
-    gen_yields_m4p5 = [0.000825665622275, 0.0250454472786, 0.322614030734, 3.41407673726]
-
-    graph_gen_m1 = ROOT.TGraph()
-    graph_gen_m3 = ROOT.TGraph()
-    graph_gen_m4p5 = ROOT.TGraph()
-
-    for pt in range(0, len(gen_coupling_m1)):
-      point = graph_gen_m1.GetN()
-      graph_gen_m1.SetPoint(point, gen_coupling_m1[pt], gen_yields_m1[pt])
-
-    graph_gen_m1.SetMarkerStyle(22)
-    graph_gen_m1.SetMarkerSize(2)
-    graph_gen_m1.SetMarkerColor(ROOT.kOrange+0)
-    graph_gen_m1.SetLineStyle(9)
-    graph_gen_m1.SetLineWidth(2)
-    graph_gen_m1.SetLineColor(ROOT.kOrange+0)
-
-    for pt in range(0, len(gen_coupling_m3)):
-      point = graph_gen_m3.GetN()
-      graph_gen_m3.SetPoint(point, gen_coupling_m3[pt], gen_yields_m3[pt])
-
-    graph_gen_m3.SetMarkerStyle(22)
-    graph_gen_m3.SetMarkerSize(2)
-    graph_gen_m3.SetMarkerColor(ROOT.kRed+1)
-    graph_gen_m3.SetLineStyle(9)
-    graph_gen_m3.SetLineWidth(2)
-    graph_gen_m3.SetLineColor(ROOT.kRed+1)
-
-    for pt in range(0, len(gen_coupling_m4p5)):
-      point = graph_gen_m4p5.GetN()
-      graph_gen_m4p5.SetPoint(point, gen_coupling_m4p5[pt], gen_yields_m4p5[pt])
-
-    graph_gen_m4p5.SetMarkerStyle(22)
-    graph_gen_m4p5.SetMarkerSize(2)
-    graph_gen_m4p5.SetMarkerColor(ROOT.kRed+4)
-    graph_gen_m4p5.SetLineStyle(9)
-    graph_gen_m4p5.SetLineWidth(2)
-    graph_gen_m4p5.SetLineColor(ROOT.kRed+4)
-
-    from samples import signal_samples_limits_m1 as samples_m1
-    from samples import signal_samples_limits_m3 as samples_m3
-    from samples import signal_samples_limits_m4p5 as samples_m4p5
-
-    graph_dummy = ROOT.TGraph() # move to TGraphAsymmErrors?
-    graph_m1 = ROOT.TGraphAsymmErrors() #ROOT.TGraph() # move to TGraphAsymmErrors?
-    graph_m3 = ROOT.TGraphAsymmErrors() #ROOT.TGraph() # move to TGraphAsymmErrors?
-    graph_m4p5 = ROOT.TGraphAsymmErrors() #ROOT.TGraph() # move to TGraphAsymmErrors?
-
-    graph_dummy.SetPoint(0, 1e-6, 1e-7)
-    graph_dummy.SetPoint(1, 1e-1, 1e10)
-    graph_dummy.SetMarkerStyle(0)
-    graph_dummy.SetMarkerSize(0)
-    graph_dummy.SetMarkerColor(0)
-    #graph_dummy.SetTitle('Event {}'.format(ientry))
-    graph_dummy.GetXaxis().SetTitle('|V^{2}|')
-    graph_dummy.GetXaxis().SetLabelSize(0.037)
-    graph_dummy.GetXaxis().SetTitleSize(0.042)
-    graph_dummy.GetXaxis().SetTitleOffset(1.1)
-    graph_dummy.GetYaxis().SetTitle('Signal yields')
-    graph_dummy.GetYaxis().SetLabelSize(0.037)
-    graph_dummy.GetYaxis().SetTitleSize(0.042)
-    graph_dummy.GetYaxis().SetTitleOffset(1.1)
-
-    print '\n mass 1'
-    for signal_file in samples_m1:
-      # get signal coupling
-      signal_mass = signal_file.mass
-      signal_ctau = signal_file.ctau
-      signal_v2 = self.tools.getVV(mass=signal_mass, ctau=signal_ctau, ismaj=True)
-
-      # compute the signal yields
-      signal_selection = 'ismatched==1' if selection=='' else 'ismatched==1 && {}'.format(selection)
-      signal_yields, err_signal_yields = ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=lumi, sigma_B=327.0e9) 
-
-      # fill graph
-      point = graph_m1.GetN()
-      graph_m1.SetPoint(point, signal_v2, signal_yields)
-      #graph_m1.SetPointError(point, 0, 0, err_signal_yields, err_signal_yields)
-      graph_m1.SetPointError(point, 0, 0, 0, 0)
-
-    graph_m1.SetMarkerStyle(20)
-    graph_m1.SetMarkerSize(2)
-    graph_m1.SetMarkerColor(ROOT.kOrange+0)
-    graph_m1.SetLineStyle(1)
-    graph_m1.SetLineWidth(2)
-    graph_m1.SetLineColor(ROOT.kOrange+0)
-
-
-    print '\n mass 3'
-    for signal_file in samples_m3:
-      # get signal coupling
-      signal_mass = signal_file.mass
-      signal_ctau = signal_file.ctau
-      signal_v2 = self.tools.getVV(mass=signal_mass, ctau=signal_ctau, ismaj=True)
-
-      # compute the signal yields
-      signal_selection = 'ismatched==1' if selection=='' else 'ismatched==1 && {}'.format(selection)
-      signal_yields, err_signal_yields = ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=lumi, sigma_B=327.0e9) 
-
-      # fill graph
-      point = graph_m3.GetN()
-      graph_m3.SetPoint(point, signal_v2, signal_yields)
-      #graph_m3.SetPointError(point, 0, 0, err_signal_yields, err_signal_yields)
-      graph_m3.SetPointError(point, 0, 0, 0, 0)
-
-    graph_m3.SetMarkerStyle(20)
-    graph_m3.SetMarkerSize(2)
-    graph_m3.SetMarkerColor(ROOT.kRed+1)
-    graph_m3.SetLineStyle(1)
-    graph_m3.SetLineWidth(2)
-    graph_m3.SetLineColor(ROOT.kRed+1)
-
-    print '\n mass 4.5'
-    for signal_file in samples_m4p5:
-      # get signal coupling
-      signal_mass = signal_file.mass
-      signal_ctau = signal_file.ctau
-      signal_v2 = self.tools.getVV(mass=signal_mass, ctau=signal_ctau, ismaj=True)
-
-      # compute the signal yields
-      signal_selection = 'ismatched==1' if selection=='' else 'ismatched==1 && {}'.format(selection)
-      signal_yields, err_signal_yields = ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=lumi, sigma_B=327.0e9) 
-
-      # fill graph
-      point = graph_m4p5.GetN()
-      graph_m4p5.SetPoint(point, signal_v2, signal_yields)
-      #graph_m4p5.SetPointError(point, 0, 0, err_signal_yields, err_signal_yields)
-      graph_m4p5.SetPointError(point, 0, 0, 0, 0)
-
-    graph_m4p5.SetMarkerStyle(20)
-    graph_m4p5.SetMarkerSize(2)
-    graph_m4p5.SetMarkerColor(ROOT.kRed+4)
-    graph_m4p5.SetLineStyle(1)
-    graph_m4p5.SetLineWidth(2)
-    graph_m4p5.SetLineColor(ROOT.kRed+4)
-
-    graph_dummy.Draw('AP')  
-    graph_m1.Draw('PL same')  
-    graph_m3.Draw('PL same')  
-    graph_m4p5.Draw('PL same')  
-    graph_gen_m1.Draw('PL same')
-    graph_gen_m3.Draw('PL same')
-    graph_gen_m4p5.Draw('PL same')
-      
-    legend = self.tools.getRootTLegend(xmin=0.15, ymin=0.55, xmax=0.45, ymax=0.9, size=0.027)
-    legend.AddEntry(graph_m1, 'm=1GeV, reco')
-    legend.AddEntry(graph_gen_m1, 'm=1GeV, gen')
-    legend.AddEntry(graph_m3, 'm=3GeV, reco')
-    legend.AddEntry(graph_gen_m3, 'm=3GeV, gen')
-    legend.AddEntry(graph_m4p5, 'm=4p5GeV, reco')
-    legend.AddEntry(graph_gen_m4p5, 'm=4p5GeV, gen')
-    legend.Draw()
-
-    if not path.exists('./myPlots/yields'):
-      os.system('mkdir -p ./myPlots/yields')
-
-    canv.SaveAs('./myPlots/yields/signal_yields_gen_reco_corr.png')
-    canv.SaveAs('./myPlots/yields/signal_yields_gen_reco_corr.pdf')
-
 
 def printInfo(opt):
   print '-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.'
@@ -755,7 +574,6 @@ if __name__ == '__main__':
     doDataMCComparison = True
     doSignalBackgroundComparison = False
     compareTwoDistributions = False
-    plotYields = False
 
     plot_log = False
     plot_categories = False
@@ -772,12 +590,6 @@ if __name__ == '__main__':
         for quantity in quantities_to_plot_small:
           plotter = Plotter(quantity=quantity, data_files=data_samples, qcd_files=qcd_samples, signal_files=signal_samples, white_list=white_list_15to300)
           plotter.plotDataMCComparison(selection=baseline_selection+'hnl_charge!=0', title='Control Region, inclusive', outdirlabel=dirlabel+'/CR/incl', branchname='flat', plot_data=True, plot_sig=False, plot_ratio=True, do_shape=False, do_luminorm=True, do_stack=False, do_log=False)
-
-
-    if plotYields:
-      from samples import signal_samples_limits_m4p5
-      plotter = Plotter(signal_files=signal_samples_limits_m4p5)
-      plotter.plotYields(lumi=41.6, selection='mu_isdsa!=1', title='', outdirlabel='')
 
 
     if doSignalBackgroundComparison:
