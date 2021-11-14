31f39e065833f9a6f231166f37773721243f1bfa
anlyon <alyon@phys.ethz.ch>
2021-11-14 14:25:10 +0100

    hlt weight and subdirlabel added in create_datacards

--- scripts/create_datacards.py
+++ scripts/create_datacards.py
@@ -19,6 +19,7 @@ def getOptions():
   from argparse import ArgumentParser
   parser = ArgumentParser(description='Script to produce the datacards', add_help=True)
   parser.add_argument('--outdirlabel'     , type=str, dest='outdirlabel'     , help='name of the outdir'                                            , default=None)
+  parser.add_argument('--subdirlabel'     , type=str, dest='subdirlabel'     , help='name of the subdir'                                            , default=None)
   #parser.add_argument('--cardlabel'       , type=str, dest='cardlabel'       , help='label of the datacard'                                         , default=None)
   parser.add_argument('--data_label'      , type=str, dest='data_label'      , help='which data samples to consider?'                               , default='V07_18Aug21')
   parser.add_argument('--qcd_label'       , type=str, dest='qcd_label'       , help='which qcd samples to consider?'                                , default='V07_18Aug21')
@@ -27,7 +28,9 @@ def getOptions():
   parser.add_argument('--categories_label', type=str, dest='categories_label', help='label of the list of categories'                               , default='standard')
   parser.add_argument('--category_label'  , type=str, dest='category_label'  , help='label of a given category within this list'                    , default=None)
   parser.add_argument('--ABCD_label'      , type=str, dest='ABCD_label'      , help='which ABCD regions?'                                           , default='cos2d_svprob')
+  parser.add_argument('--weight_hlt'      , type=str, dest='weight_hlt'      , help='name of the branch of hlt weight'                              , default='weight_hlt_A1')
   #parser.add_argument('--white_list '     , type=str, dest='white_list'      , help='pthat range to consider for qcd samples'                       , default='')
+  parser.add_argument('--add_weight_hlt'  ,           dest='add_weight_hlt'  , help='add hlt weight'                           , action='store_true', default=False)
   parser.add_argument('--do_categories'   ,           dest='do_categories'   , help='compute yields in categories'             , action='store_true', default=False)
   parser.add_argument('--add_Bc'          ,           dest='add_Bc'          , help='add the Bc samples'                       , action='store_true', default=False)
   #parser.add_argument('--submit_batch', dest='submit_batch', help='submit on the batch?', action='store_true', default=False)
@@ -56,11 +59,12 @@ def printInfo(opt):
   print ' selection:       {}'.format(opt.selection_label)
   print '\n'
   print ' outdir label:    {}'.format(opt.outdirlabel)
+  print ' subdir label:    {}'.format(opt.subdirlabel)
   print '-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.'
   print '\n'
 
 class DatacardsMaker(Tools):
-  def __init__(self, data_file='', signal_files='', baseline_selection='', ABCD_regions='', do_categories=True, categories=None, category_label=None, add_Bc=False, outdirlabel=''):
+  def __init__(self, data_file='', signal_files='', baseline_selection='', ABCD_regions='', do_categories=True, categories=None, category_label=None, weight_hlt=None, add_weight_hlt=True, add_Bc=False, outdirlabel='', subdirlabel=''):
     self.tools = Tools()
     self.data_file = data_file
     self.signal_files = signal_files 
@@ -71,8 +75,10 @@ class DatacardsMaker(Tools):
     if do_categories and categories == None:
       raise RuntimeError('Please indicate which categories dictionnary to use')
     self.category_label = category_label
+    self.weight_hlt = weight_hlt
+    self.add_weight_hlt = add_weight_hlt
     self.add_Bc = add_Bc
-    self.outputdir = '../outputs/{}/datacards'.format(outdirlabel)
+    self.outputdir = '../outputs/{}/datacards/{}'.format(outdirlabel, subdirlabel)
     if not path.exists(self.outputdir):
       os.system('mkdir -p {}'.format(self.outputdir))
 
@@ -108,6 +114,7 @@ class DatacardsMaker(Tools):
 
     # ABCD method
     background_yields = ComputeYields(data_file=data_file, selection=background_selection).computeBkgYieldsFromABCDData(mass=mass, resolution=resolution, ABCD_regions=self.ABCD_regions)[0] 
+    #background_yields = ComputeYields(data_file=data_file, selection=background_selection).computeBkgYieldsFromABCDHybrid(mass=mass, resolution=resolution, ABCD_regions=self.ABCD_regions)[0] 
 
     # TF method
     #from samples import qcd_samples
@@ -119,7 +126,7 @@ class DatacardsMaker(Tools):
 
     #TODO add flag that indicates to scale to given lumi
     #if background_yields != 1e-9: background_yields = background_yields * 41.6/(0.774 * 0.1) # added 10percent since we only use Chunk2
-    if background_yields != 1e-9: background_yields = background_yields * 41.6/0.774
+    if background_yields != 1e-9: background_yields = background_yields * 41.6/0.774 #TODO add in the config the target lumi and the lumi of the dataset
 
     return background_yields
 
@@ -130,9 +137,9 @@ class DatacardsMaker(Tools):
       category_selection = category.definition_flat + '&& ' + category.cutbased_selection
       signal_selection += ' && {}'.format(category_selection)
 
-    signal_yields = ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=41.6, isBc=False)[0] 
+    signal_yields = ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=41.6, sigma_B=472.8e9, add_weight_hlt=self.add_weight_hlt, weight_hlt=self.weight_hlt, isBc=False)[0] #TODO add sigma_B and lumi in the config
     if self.add_Bc and signal_file.filename_Bc != '':
-      signal_yields += ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=41.6, isBc=True)[0]
+      signal_yields += ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=41.6, sigma_B=472.8e9, add_weight_hlt=self.add_weight_hlt, weight_hlt=self.weight_hlt, isBc=True)[0]
 
       #print 'yields {} + {} = {}'.format(ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=41.6, isBc=False)[0], ComputeYields(signal_file=signal_file, selection=signal_selection).computeSignalYields(lumi=41.6, isBc=True), signal_yields)[0]
 
@@ -245,10 +252,14 @@ if __name__ == '__main__':
     signal_files = signal_samples[opt.signal_label]
 
     outdirlabel = opt.outdirlabel
+    subdirlabel = opt.subdirlabel
 
     categories = categories[opt.categories_label]
     baseline_selection = selection[opt.selection_label].flat
     ABCD_regions = ABCD_regions[opt.ABCD_label]
+
+    add_weight_hlt = opt.add_weight_hlt
+    weight_hlt = opt.weight_hlt
 
     do_categories = opt.do_categories
     add_Bc = opt.add_Bc
@@ -261,8 +272,11 @@ if __name__ == '__main__':
         categories = categories, 
         category_label = opt.category_label, 
         ABCD_regions = ABCD_regions,
+        weight_hlt = weight_hlt,
+        add_weight_hlt = add_weight_hlt,
         add_Bc = add_Bc, 
         outdirlabel = outdirlabel,
+        subdirlabel = subdirlabel,
     ).process() 
 
   else:
@@ -272,10 +286,10 @@ if __name__ == '__main__':
     For an interactive usage of the script, use the code below
     '''
     #data_file = data_samples['V07_18Aug21'][0]
-    data_file = data_samples['V05_29Jun21'][0]
+    data_file = data_samples['V08_29Sep21'][0]
 
-    categories = categories['standard']
-    baseline_selection = selection['mass_charge'].flat
+    categories = categories['13Oct21_slimmed']
+    baseline_selection = selection['13Oct21'].flat
     #categories['lxy0to1_OS'] = 'sv_lxy<1 && trgmu_charge!=mu_charge && b_mass>2.7'
     #categories['lxy1to5_OS'] = 'sv_lxy>=1 && sv_lxy<5 && trgmu_charge!=mu_charge && b_mass>1.7 && pi_pt>1 && trgmu_pi_mass<4.5 '
     #categories['lxygt5_OS'] = 'sv_lxy>=5 && trgmu_charge!=mu_charge && b_mass>1.5 && deltar_mu_pi<1.5 && deltar_trgmu_pi<1 && pi_pt>2 && trgmu_pi_mass<4.5'
@@ -324,7 +338,7 @@ if __name__ == '__main__':
     dirlabel = 'test'
 
     ABCD_regions = ABCD_regions['cos2d_svprob'] 
-    signal_files = signal_samples['limits_m1']
+    signal_files = signal_samples['limits_m1_29Sep21']
     datacards = DatacardsMaker(data_file=data_file, signal_files=signal_files, baseline_selection=baseline_selection, ABCD_regions=ABCD_regions, do_categories=True, categories=categories, add_Bc=False, outdirlabel=dirlabel).process() 
 
     #signal_files = signal_samples['limits_m3']
