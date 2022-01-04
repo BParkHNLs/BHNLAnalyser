29093e771289fc79701cc5f570e29ea2a30c5c2a
anlyon <alyon@phys.ethz.ch>
2022-01-04 10:06:13 +0100

    further ABCD regions added

--- objects/ABCD_regions.py
+++ objects/ABCD_regions.py
@@ -21,6 +21,14 @@ ABCD_regions['cos2d_svprob'] = ABCD(
     CR_D_selection = 'hnl_cos2d < 0.993 && sv_prob < 0.05'
     )
 
+ABCD_regions['cos2d_svprob_0p996'] = ABCD(
+    label = 'cos2d_svprob_0p996',
+    SR_selection = 'hnl_cos2d > 0.996 && sv_prob > 0.05',
+    CR_B_selection = 'hnl_cos2d > 0.996 && sv_prob < 0.05',
+    CR_C_selection = 'hnl_cos2d < 0.996 && sv_prob > 0.05',
+    CR_D_selection = 'hnl_cos2d < 0.996 && sv_prob < 0.05'
+    )
+
 ABCD_regions['bmass_hnlcharge'] = ABCD(
     label = 'bmass_hnlcharge',
     SR_selection = 'b_mass < 6.27 && hnl_charge == 0',
@@ -29,3 +37,10 @@ ABCD_regions['bmass_hnlcharge'] = ABCD(
     CR_D_selection = 'b_mass > 6.27 && hnl_charge != 0'
     )
 
+ABCD_regions['cos2d_hnlcharge'] = ABCD(
+    label = 'cos2d_hnlcharge',
+    SR_selection = 'hnl_cos2d > 0.998 && hnl_charge == 0',
+    CR_B_selection = 'hnl_cos2d > 0.998 && hnl_charge != 0',
+    CR_C_selection = 'hnl_cos2d < 0.998 && hnl_charge == 0',
+    CR_D_selection = 'hnl_cos2d < 0.998 && hnl_charge != 0'
+    )
