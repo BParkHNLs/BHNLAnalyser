e9e867157c91a55443b0bb5ed22a92e926917868
anlyon <alyon@phys.ethz.ch>
2022-01-07 13:56:47 +0100

    example_cfg.py modified

--- cfgs/example_cfg.py
+++ cfgs/example_cfg.py
@@ -9,28 +9,35 @@ cfg = Config(
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
 
     # ---     SAMPLES     --- #
-    data_label = 'V07_18Aug21',
-    qcd_label = 'V07_18Aug21',
-    signal_label = 'private',
+    data_label = 'V09_06Nov21',
+    qcd_label = 'V09_06Nov21',
+    signal_labels = ['central_V09_06Nov21_m1', 'central_V09_06Nov21_m3', 'central_V09_06Nov21_m4p5'],
     sample_type = 'flat',
     tree_name = 'signal_tree',
+    qcd_white_list = '20to300',
     # ----------------------- #
 
     # ---    CATEGORIES   --- #
-    categories_label = 'standard',
+    categories_label = '3cat_0_1_5_significance',
     # ----------------------- #
 
     # ---    SELECTION    --- #
-    selection_label = 'standard',
+    selection_label = 'study_Nov21',
     # ----------------------- #
 
-    # add weights
+    # ---     WEIGHTS     --- #
+    add_weight_hlt = True,
+    add_weight_pu = True,
+    branch_weight_hlt = 'weight_hlt_HLT_Mu9_IP6_A1_6',
+    branch_weight_pu = 'weight_pu_qcd_A',
+    # ----------------------- #
+
 
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
     ###   Plotting Info     ###
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
-    # quantities
-    quantities_label = 'small',
+    # quantities 
+    quantities_label = ['all', 'muonId_study_displacedmuon', 'trackId'],
     # what to plot
     plot_CR = True,
     plot_SR = False,
@@ -38,9 +45,10 @@ cfg = Config(
     # plot style
     do_shape = True,
     do_luminorm = False,
-    do_stack = False,
+    do_stack = True,
     do_log = False,
-    add_overflow = False,
+    plot_ratio = False,
+    add_overflow = False, 
     add_CMSlabel = True,
     CMStag = 'Preliminary',
 
@@ -48,10 +56,21 @@ cfg = Config(
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
     ###    Datacard info    ###
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
-    ABCD_label = 'cos2d_svprob',
+    ABCD_label = 'cos2d_svprob_0p996',
+    do_ABCD = False,
+    do_ABCDHybrid = True,
+    do_TF = False,
     do_categories = True,
     add_Bc = False,
+    lumi_target = 41.6,
+    sigma_B = 472.8e9,
+    sigma_mult_window = 2,
 
+
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#
+    ###     Limits info     ###
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#
+    run_blind = True,
 )
 
 
