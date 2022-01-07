c7c3c5c1c80f3824ebd911693e0954162c7df60a
anlyon <alyon@phys.ethz.ch>
2022-01-07 14:14:58 +0100

    further ranges added in qcd_white_list

--- objects/qcd_white_list.py
+++ objects/qcd_white_list.py
@@ -21,3 +21,35 @@ white_list['20to300'] = [
   'pt120to170', 
   'pt170to300', 
 ]
+
+white_list['20to170'] = [
+  'pt20to30', 
+  'pt30to50', 
+  'pt50to80', 
+  'pt80to120', 
+  'pt120to170', 
+]
+
+white_list['20to120'] = [
+  'pt20to30', 
+  'pt30to50', 
+  'pt50to80', 
+  'pt80to120', 
+]
+
+white_list['20to80'] = [
+  'pt20to30', 
+  'pt30to50', 
+  'pt50to80', 
+]
+
+white_list['50to300'] = [
+  'pt50to80', 
+  'pt80to120', 
+  'pt120to170', 
+  'pt170to300', 
+]
+
+white_list['30to50'] = [
+  'pt30to50', 
+]
