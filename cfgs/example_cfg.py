f6050a2bfb4b40d4293fad2aa896a2a150f4a946
anlyon <alyon@phys.ethz.ch>
2022-05-11 17:24:52 +0200

    example_cfg.py updated

--- cfgs/example_cfg.py
+++ cfgs/example_cfg.py
@@ -9,16 +9,16 @@ cfg = Config(
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
 
     # ---     SAMPLES     --- #
-    data_label = 'V10_30Dec21',
-    qcd_label = 'V10_30Dec21',
-    signal_labels = ['V10_30Dec21_m1', 'V10_30Dec21_m1p5', 'V10_30Dec21_m2', 'V10_30Dec21_m3', 'V10_30Dec21_m4p5'],
+    data_label = 'V11_24Apr22',
+    qcd_label = 'V11_24Apr22',
+    signal_labels = ['V11_24Apr22_m1_large', 'V11_24Apr22_m1p5_large', 'V11_24Apr22_m2_large', 'V11_24Apr22_m3_large', 'V11_24Apr22_m4p5_large'],
     sample_type = 'flat',
     tree_name = 'signal_tree',
     qcd_white_list = '20to300',
     # ----------------------- #
 
     # ---    CATEGORIES   --- #
-    categories_label = '3cat_0_1_5_significance',
+    categories_label = 'V11_24Apr22_permass',
     # ----------------------- #
 
     # ---    SELECTION    --- #
@@ -26,10 +26,11 @@ cfg = Config(
     # ----------------------- #
 
     # ---     WEIGHTS     --- #
-    add_weight_hlt = False,
+    add_weight_hlt = True,
     add_weight_pu = True,
     branch_weight_hlt = 'weight_hlt_HLT_Mu9_IP6_A1_6',
-    branch_weight_pu = 'weight_pu_qcd_D',
+    branch_weight_puqcd = 'weight_pu_qcd_D',
+    branch_weight_pusig = 'weight_pu_sig_D',
     # ----------------------- #
 
 
@@ -48,7 +49,7 @@ cfg = Config(
     do_stack = False,
     do_log = False,
     plot_ratio = False,
-    add_overflow = True, 
+    add_overflow = False, 
     add_CMSlabel = True,
     add_lumilabel = True,
     CMStag = 'Preliminary',
@@ -61,28 +62,32 @@ cfg = Config(
     do_counting = False,
     do_shape_analysis = True,
     do_shape_TH1 = False,
-    lumi_target = 41.6,
+    lumi_target = 41.601,
 
     # if counting, or shape_TH1, choose bkg estimation method
     do_ABCD = False,
-    do_ABCDHybrid = True,
+    do_ABCDHybrid = False,
     do_TF = False,
-    do_realData = False,
+    do_realData = True,
     ABCD_label = 'cos2d_svprob_0p996',
     sigma_mult_window = 2,
 
     # if shape analysis
     signal_model_label = 'voigtian',
+    use_discrete_profiling = True,
+    ## if not discrete profiling, choose fixed background model
     background_model_label = 'chebychev',
     do_binned_fit = True,
     do_blind = True,
-    nbins = 50, 
-    plot_pulls = True,
+    mass_window_size = 3,
+    fit_window_size = 10,
+    nbins = 100, 
+    plot_pulls = False,
+    plot_prefit = True,
 
     # further options
     do_categories = True,
     add_Bc = False,
-    plot_prefit = True,
     sigma_B = 472.8e9,
 
 
