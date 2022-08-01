06ca9be246acf175090f79f7588aca80067e1fc1
anlyon <alyon@phys.ethz.ch>
2022-08-01 21:19:39 +0200

    resolutions.py added

---
+++ objects/resolutions.py
@@ -0,0 +1,11 @@
+'''
+  List of the resolution for each mass
+'''
+
+resolutions = {}
+resolutions[1.0] = 0.00873
+resolutions[1.5] = 0.0129
+resolutions[2.0] = 0.0169
+resolutions[3.0] = 0.0248
+resolutions[4.5] = 0.0392
+
