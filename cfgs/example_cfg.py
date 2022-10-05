ddfd3b1132e7370e752e0036027b6057144ced27
anlyon <alyon@phys.ethz.ch>
2022-10-05 20:51:11 +0200

    example cfg modified

--- cfgs/example_cfg.py
+++ cfgs/example_cfg.py
@@ -18,15 +18,15 @@ cfg = Config(
     # ----------------------- #
 
     # ---     POINTS      --- #
-    points_label = 'generated',
+    points_label = 'baseline',
     # ----------------------- #
 
     # ---   REWEIGHTING   --- #
-    reweighting_strategy = 'exclusive_fromlargerctau',
+    reweighting_strategy = 'inclusive',
     # ----------------------- #
 
     # ---    CATEGORIES   --- #
-    categories_label = 'V12_08Aug22_permass',
+    categories_label = 'categories_0_50_150',
     # ----------------------- #
 
     # ---    SELECTION    --- #
@@ -35,8 +35,9 @@ cfg = Config(
     do_mva = True,
 
     # if mva selection
-    training_label = 'test_20Aug2022_13h40m03s',
-    cut_score = 0.9,
+    do_parametric = True,
+    training_label = 'test_2022Sep27_16h35m02s',
+    cut_score = 0.99,
     # ----------------------- #
 
     # ---     WEIGHTS     --- #
@@ -55,18 +56,18 @@ cfg = Config(
     ###   Plotting Info     ###
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
     # quantities 
-    quantities_label = ['all'],
+    quantities_label = ['all', 'muonId_study_displacedmuon', 'trackId'],
     # what to plot
-    plot_CR = True,
+    plot_CR = False,
     plot_SR = False,
-    plot_dataSig = False,
+    plot_dataSig = True,
     # plot style
     do_shape = True,
     do_luminorm = False,
     do_stack = False,
-    do_log = False,
+    do_log = True,
     plot_ratio = False,
-    add_overflow = False, 
+    add_overflow = True, 
     add_CMSlabel = True,
     add_lumilabel = False,
     CMStag = '"Preliminary"',
@@ -100,7 +101,7 @@ cfg = Config(
     fit_window_size = 10,
     nbins = 100, 
     plot_pulls = False,
-    plot_prefit = False,
+    plot_prefit = True,
 
     # further options
     do_categories = True,
@@ -108,6 +109,8 @@ cfg = Config(
     sigma_B = 472.8e9,
     lhe_efficiency = 0.08244,
     do_tdrstyle = False,
+    resolution_p0 = 0.0002747, 
+    resolution_p1 = 0.008302, 
 
 
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
