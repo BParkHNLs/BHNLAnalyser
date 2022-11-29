ee07e045061bd99f99677cf527ccc4d206ff1045
anlyon <alyon@phys.ethz.ch>
2022-11-29 17:33:50 +0100

    write efficiency table implemented (AN-v3)

--- scripts/write_yields_table.py
+++ scripts/write_yields_table.py
@@ -4,18 +4,14 @@ import ROOT
 from collections import OrderedDict
 
 from tools import Tools
+from compute_yields import ComputeYields
 sys.path.append('../objects')
 from categories import categories
+from samples import data_samples
+from baseline_selection import selection
+from quantity import Quantity
 
 tools = Tools()
-
-output_label = 'V12_08Aug22' 
-tag = 'study_categorisation_0_50_150_new_features_v2_score0p95' 
-path = '../outputs/{}/datacards/{}'.format(output_label, tag)
-
-masses = [1.0, 1.5, 2.0, 3.0, 4.5]
-ctaus = [1.0, 10.0, 100.0, 1000.0]
-categories = categories['categories_0_50_150']
 
 
 def getSignalYields(line):
@@ -39,61 +35,179 @@ def getCategoryTitle(category):
   title = title.replace('$$', '')
   return title
 
-table_yields = open('table_yields.txt', 'w+')
 
-for mass in masses:
-  coupling_line = '{} & &'.format(mass)
-  for ictau, ctau in enumerate(ctaus):
-    v2 = tools.getVV(mass, ctau)
-    coupling = tools.getCouplingLabel(v2)
-    if ictau != len(ctaus)-1:
-      coupling_line += ' & {}'.format(coupling)
-    else:
-      coupling_line += ' & {} \\\ '.format(coupling)
-  table_yields.write('\hline')
-  table_yields.write('\n' + coupling_line)
-  table_yields.write('\n' + '\hline')
+def writeYieldsTable():
+  table_yields = open('table_yields.txt', 'w+')
 
-  for icat, category in enumerate(categories): 
-    if 'incl' in category.label: continue
-    signal_yields = OrderedDict()
-    data_obs_name = 'workspace_data_obs_bhnl_m_{}_cat_{}.root'.format(str(mass).replace('.', 'p'), category.label)
-    try:
-      data_obs_file = tools.getRootFile('{}/{}'.format(path, data_obs_name))
-      workspace = data_obs_file.Get("workspace")
-      background_yields = getBackgroundYields(workspace)
-    except:
-      background_yields = '-'
-    table_entry = ' & {} & {}'.format(getCategoryTitle(category), background_yields)
-
-    for ctau in ctaus:
+  for mass in masses:
+    coupling_line = '{} & &'.format(mass)
+    for ictau, ctau in enumerate(ctaus):
       v2 = tools.getVV(mass, ctau)
       coupling = tools.getCouplingLabel(v2)
-
-      datacard_name = 'datacard_bhnl_m_{}_v2_{}_cat_{}.txt'.format(str(mass).replace('.', 'p'), str(coupling).replace('.', 'p').replace('-', 'm'), category.label)
-      try:
-        card = open('{}/{}'.format(path, datacard_name))
-        lines = card.readlines()
-        for line in lines:
-          if 'rate' not in line: continue
-          signal_yields[ctau] = getSignalYields(line)
-      except:
-          signal_yields[ctau] = '-'
-
-    for ictau, ctau in enumerate(ctaus):
-      if ictau != len(ctaus)-1: 
-        table_entry += ' & {}'.format(signal_yields[ctau])
+      if ictau != len(ctaus)-1:
+        coupling_line += ' & {}'.format(coupling)
       else:
-        table_entry += ' & {} \\\ '.format(signal_yields[ctau])
-    table_yields.write('\n' + table_entry)
-    if icat == len(categories)-1:
-      table_yields.write('\n' + '\hline')
+        coupling_line += ' & {} \\\ '.format(coupling)
+    table_yields.write('\hline')
+    table_yields.write('\n' + coupling_line)
+    table_yields.write('\n' + '\hline')
+
+    for icat, category in enumerate(categories): 
+      if 'incl' in category.label: continue
+      signal_yields = OrderedDict()
+      data_obs_name = 'workspace_data_obs_bhnl_m_{}_cat_{}.root'.format(str(mass).replace('.', 'p'), category.label)
+      try:
+        data_obs_file = tools.getRootFile('{}/{}'.format(path, data_obs_name))
+        workspace = data_obs_file.Get("workspace")
+        background_yields = getBackgroundYields(workspace)
+      except:
+        background_yields = '-'
+      table_entry = ' & {} & {}'.format(getCategoryTitle(category), background_yields)
+
+      for ctau in ctaus:
+        v2 = tools.getVV(mass, ctau)
+        coupling = tools.getCouplingLabel(v2)
+
+        datacard_name = 'datacard_bhnl_m_{}_v2_{}_cat_{}.txt'.format(str(mass).replace('.', 'p'), str(coupling).replace('.', 'p').replace('-', 'm'), category.label)
+        try:
+          card = open('{}/{}'.format(path, datacard_name))
+          lines = card.readlines()
+          for line in lines:
+            if 'rate' not in line: continue
+            signal_yields[ctau] = getSignalYields(line)
+        except:
+            signal_yields[ctau] = '-'
+
+      for ictau, ctau in enumerate(ctaus):
+        if ictau != len(ctaus)-1: 
+          table_entry += ' & {}'.format(signal_yields[ctau])
+        else:
+          table_entry += ' & {} \\\ '.format(signal_yields[ctau])
+      table_yields.write('\n' + table_entry)
+      if icat == len(categories)-1:
+        table_yields.write('\n' + '\hline')
 
 
-table_yields.close()
-print '-> table_yields.txt created'
-os.system('cat table_yields.txt')
+  table_yields.close()
+  print '-> table_yields.txt created'
+  os.system('cat table_yields.txt')
 
-    
+
+def writeEfficiencyTable():
+  table_efficiency = open('table_efficiency.txt', 'w+')
+
+  for mass in masses:
+    coupling_line = '{} & &'.format(mass)
+    for ictau, ctau in enumerate(ctaus):
+      v2 = tools.getVV(mass, ctau)
+      coupling = tools.getCouplingLabel(v2)
+      if ictau != len(ctaus)-1:
+        coupling_line += ' & $|$V$^2|$={}'.format(coupling)
+      else:
+        coupling_line += ' & $|$V$^2|$={} \\\ '.format(coupling)
+    table_efficiency.write('\hline')
+    table_efficiency.write('\n' + coupling_line)
+    table_efficiency.write('\n' + '\hline')
+
+    for icat, category in enumerate(categories): 
+      if 'incl' in category.label: continue
+
+      # get the number of background yields before the pNN cut
+      resolution = resolution_p0 + mass * resolution_p1
+      hnl_mass = Quantity(name_flat='hnl_mass', label='hnl_mass', nbins=80, bin_min=mass-2*resolution, bin_max=mass+2*resolution)
+
+      f_data = tools.getRootFile(data_sample.filename) 
+      tree_data = tools.getTree(f_data, 'signal_tree')
+      background_selection = baseline_selection + ' && ' + category.definition_flat + ' && hnl_charge==0'
+      hist_data = tools.createHisto(tree=tree_data, quantity=hnl_mass, selection=background_selection)
+      
+      background_yields_ini = hist_data.Integral() * 41.6 / 5.302
+
+      # get the number of background yields after the pNN cut
+      data_obs_name = 'workspace_data_obs_bhnl_m_{}_cat_{}.root'.format(str(mass).replace('.', 'p'), category.label)
+      try:
+        data_obs_file = tools.getRootFile('{}/{}'.format(path, data_obs_name))
+        workspace = data_obs_file.Get("workspace")
+        background_yields_sel = float(getBackgroundYields(workspace))
+      except:
+        background_yields_sel = '-'
+      #print '{} ini: {} sel: {}'.format(category.label, background_yields_ini, background_yields_sel)
+
+      if background_yields_sel != '-':
+        efficiency_background = round(background_yields_sel / background_yields_ini * 100, 2)
+      else:
+        efficiency_background = '-'
+      #print efficiency_background
+
+      table_entry = ' & {} & {}\%'.format(getCategoryTitle(category), efficiency_background)
+
+      signal_yields_ini = OrderedDict()
+      signal_yields_sel = OrderedDict()
+      efficiency_signal = OrderedDict()
+      for ctau in ctaus:
+        # get the number of signal yields before the pNN cut
+        signal_selection = 'ismatched==1 && ' + baseline_selection + ' && ' + category.definition_flat
+        signal_yields_ini[ctau] = ComputeYields(signal_label=signal_label, selection=signal_selection).computeSignalYields(mass=mass, ctau=ctau, lumi=41.6, sigma_B=472.8e9, isBc=False, strategy='inclusive', add_weight_hlt=True, add_weight_pu=True, add_weight_muid=True, weight_hlt='weight_hlt_D1_tag_fired_HLT_Mu9_IP6_or_HLT_Mu12_IP6_ptdxysigbs_max5e6_v2_smalltable_v2', weight_pusig='weight_pu_sig_D', weight_mu0id='weight_mu0_softid', weight_muid='weight_mu_looseid')[0] 
+
+        # get the number of signal yields after the pNN cut
+        v2 = tools.getVV(mass, ctau)
+        coupling = tools.getCouplingLabel(v2)
+
+        datacard_name = 'datacard_bhnl_m_{}_v2_{}_cat_{}.txt'.format(str(mass).replace('.', 'p'), str(coupling).replace('.', 'p').replace('-', 'm'), category.label)
+        try:
+          card = open('{}/{}'.format(path, datacard_name))
+          lines = card.readlines()
+          for line in lines:
+            if 'rate' not in line: continue
+            signal_yields_sel[ctau] = float(getSignalYields(line))
+        except:
+            signal_yields_sel[ctau] = '-'
+        #print '{} {} ini: {} sel: {}'.format(category.label, ctau, signal_yields_ini[ctau], signal_yields_sel[ctau])
+
+        if signal_yields_sel[ctau] != '-':
+          efficiency_signal[ctau] = round(signal_yields_sel[ctau] / signal_yields_ini[ctau] * 100, 2)
+          efficiency_signal[ctau] = str(efficiency_signal[ctau]) + '\%'
+        else:
+          efficiency_signal[ctau] = '-'
+        #print '{} {} {}'.format(getCategoryTitle(category), ctau, efficiency_signal[ctau])
+
+      for ictau, ctau in enumerate(ctaus):
+        if ictau != len(ctaus)-1: 
+          table_entry += ' & {}'.format(efficiency_signal[ctau])
+        else:
+          table_entry += ' & {} \\\ '.format(efficiency_signal[ctau])
+
+      table_efficiency.write('\n' + table_entry)
+      if icat == len(categories)-1:
+        table_efficiency.write('\n' + '\hline')
+
+
+  table_efficiency.close()
+  print '-> table_efficiency.txt created'
+  os.system('cat table_efficiency.txt')
+      
+
+if __name__ == '__main__':
+
+  output_label = 'V12_08Aug22' 
+  tag = 'study_categorisation_0_50_150_new_features_v2_score0p95' 
+  path = '../outputs/{}/datacards/{}'.format(output_label, tag)
+
+  masses = [1.0, 1.5, 2.0, 3.0, 4.5]
+  ctaus = [1.0, 10.0, 100.0, 1000.0]
+  categories = categories['categories_0_50_150']
+
+  data_label = 'V12_08Aug22'
+  data_sample = data_samples[data_label][0]
+
+  baseline_selection = selection['baseline_08Aug22'].flat
+
+  resolution_p0 = 0.0002747
+  resolution_p1 = 0.008302 
+
+  signal_label = 'V12_08Aug22_training_large'
+
+  #writeYieldsTable()
+  writeEfficiencyTable()
 
 
