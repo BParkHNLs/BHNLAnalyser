bd915b1d192ef320f8d1e54b1d7d6c2cf1cf664f
anlyon <alyon@phys.ethz.ch>
2023-04-26 20:28:13 +0200

    example_cfg modified

--- cfgs/example_cfg.py
+++ cfgs/example_cfg.py
@@ -9,16 +9,121 @@ cfg = Config(
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
 
     # ---     SAMPLES     --- #
-    data_label = 'V12_08Aug22',
-    qcd_label = 'V12_08Aug22',
-    signal_labels = ['V12_08Aug22_m1', 'V12_08Aug22_m1p5', 'V12_08Aug22_m2', 'V12_08Aug22_m3', 'V12_08Aug22_m4p5'],
+    data_label = 'V13_06Feb23_fullBPark',
+    qcd_label = 'V13_06Feb23',
+    signal_labels = [
+      'V13_06Feb23_m1',
+      'V13_06Feb23_m1p5',
+      'V13_06Feb23_m2',
+      'V13_06Feb23_m3',
+      'V13_06Feb23_m4p5',
+      'V13_06Feb23_m5p5',
+      'V42_06Feb23_m1p02',
+      'V42_06Feb23_m1p04',
+      'V42_06Feb23_m1p06',
+      'V42_06Feb23_m1p08',
+      'V42_06Feb23_m1p1',
+      'V42_06Feb23_m1p12',
+      #'V42_06Feb23_m1p14',
+      'V42_06Feb23_m1p16',
+      'V42_06Feb23_m1p18',
+      'V42_06Feb23_m1p2',
+      'V42_06Feb23_m1p22',
+      'V42_06Feb23_m1p24',
+      'V42_06Feb23_m1p26',
+      'V42_06Feb23_m1p28',
+      'V42_06Feb23_m1p3',
+      'V42_06Feb23_m1p32',
+      'V42_06Feb23_m1p34',
+      'V42_06Feb23_m1p36',
+      'V42_06Feb23_m1p38',
+      'V42_06Feb23_m1p4',
+      'V42_06Feb23_m1p42',
+      'V42_06Feb23_m1p44',
+      'V42_06Feb23_m1p46',
+      'V42_06Feb23_m1p48',
+      'V42_06Feb23_m1p53',
+      'V42_06Feb23_m1p56',
+      'V42_06Feb23_m1p59',
+      #'V42_06Feb23_m1p62',
+      'V42_06Feb23_m1p65',
+      #'V42_06Feb23_m1p68',
+      'V42_06Feb23_m1p71',
+      'V42_06Feb23_m1p74',
+      'V42_06Feb23_m1p77',
+      'V42_06Feb23_m1p8',
+      'V42_06Feb23_m1p83',
+      #'V42_06Feb23_m1p86',
+      'V42_06Feb23_m1p89',
+      'V42_06Feb23_m1p92',
+      'V42_06Feb23_m1p95',
+      #'V42_06Feb23_m1p98',
+      'V42_06Feb23_m2p05',
+      'V42_06Feb23_m2p1',
+      'V42_06Feb23_m2p15',
+      'V42_06Feb23_m2p2',
+      'V42_06Feb23_m2p25',
+      'V42_06Feb23_m2p3',
+      'V42_06Feb23_m2p35',
+      'V42_06Feb23_m2p4',
+      'V42_06Feb23_m2p45',
+      'V42_06Feb23_m2p5',
+      'V42_06Feb23_m2p55',
+      'V42_06Feb23_m2p6',
+      'V42_06Feb23_m2p65',
+      'V42_06Feb23_m2p7',
+      'V42_06Feb23_m2p75',
+      'V42_06Feb23_m2p8',
+      'V42_06Feb23_m2p85',
+      'V42_06Feb23_m2p9',
+      'V42_06Feb23_m2p95',
+      'V42_06Feb23_m3p05',
+      'V42_06Feb23_m3p1',
+      'V42_06Feb23_m3p15',
+      'V42_06Feb23_m3p2',
+      'V42_06Feb23_m3p25',
+      'V42_06Feb23_m3p3',
+      'V42_06Feb23_m3p35',
+      'V42_06Feb23_m3p4',
+      'V42_06Feb23_m3p45',
+      'V42_06Feb23_m3p5',
+      'V42_06Feb23_m3p55',
+      'V42_06Feb23_m3p6',
+      'V42_06Feb23_m3p65',
+      'V42_06Feb23_m3p7',
+      'V42_06Feb23_m3p75',
+      'V42_06Feb23_m3p8',
+      'V42_06Feb23_m3p85',
+      'V42_06Feb23_m3p9',
+      'V42_06Feb23_m3p95',
+      'V42_06Feb23_m4p0',
+      'V42_06Feb23_m4p1',
+      'V42_06Feb23_m4p2',
+      #'V42_06Feb23_m4p3',
+      #'V42_06Feb23_m4p4',
+      'V42_06Feb23_m4p6',
+      'V42_06Feb23_m4p7',
+      'V42_06Feb23_m4p8',
+      'V42_06Feb23_m4p9',
+      'V42_06Feb23_m5p0',
+      'V42_06Feb23_m5p1',
+      'V42_06Feb23_m5p2',
+      'V42_06Feb23_m5p3',
+      'V42_06Feb23_m5p4',
+      'V42_06Feb23_m5p5',
+      'V42_06Feb23_m5p6',
+      'V42_06Feb23_m5p7',
+      'V42_06Feb23_m5p8',
+      'V42_06Feb23_m5p9',
+      'V42_06Feb23_m6p0',
+    ],
     sample_type = 'flat',
     tree_name = 'signal_tree',
     qcd_white_list = '20to300',
     # ----------------------- #
 
     # ---     POINTS      --- #
-    points_label = 'baseline',
+    ctau_points_label = 'baseline',
     # ----------------------- #
 
     # ---   REWEIGHTING   --- #
@@ -26,7 +131,7 @@ cfg = Config(
     # ----------------------- #
 
     # ---    CATEGORIES   --- #
-    categories_label = 'categories_0_50_150',
+    categories_label = 'categories_0_50_150_Bc',
     # ----------------------- #
 
     # ---    SELECTION    --- #
@@ -36,7 +141,7 @@ cfg = Config(
 
     # if mva selection
     do_parametric = True,
-    training_label = 'test_2022Sep27_16h35m02s',
+    training_label = 'V13_06Feb23_2023Apr06_14h13m31s',
     cut_score = 0.99,
     # ----------------------- #
 
@@ -44,9 +149,9 @@ cfg = Config(
     add_weight_hlt = True,
     add_weight_pu = True,
     add_weight_muid = True,
-    branch_weight_hlt = 'weight_hlt_D1_tag_fired_HLT_Mu9_IP6_or_HLT_Mu12_IP6_ptdxysigbs_max5e6_v2_smalltable_v2',
+    branch_weight_hlt = 'weight_hlt_fullBpark',
     branch_weight_puqcd = 'weight_pu_qcd_D',
-    branch_weight_pusig = 'weight_pu_sig_D',
+    branch_weight_pusig = 'weight_pu_sig_tot',
     branch_weight_mu0id = 'weight_mu0_softid',
     branch_weight_muid = 'weight_mu_looseid',
     # ----------------------- #
@@ -56,18 +161,18 @@ cfg = Config(
     ###   Plotting Info     ###
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
     # quantities 
-    quantities_label = ['all', 'muonId_study_displacedmuon', 'trackId'],
+    quantities_label = ['small'],
     # what to plot
-    plot_CR = False,
+    plot_CR = True,
     plot_SR = False,
-    plot_dataSig = True,
+    plot_dataSig = False,
     # plot style
     do_shape = True,
     do_luminorm = False,
     do_stack = False,
-    do_log = True,
+    do_log = False,
     plot_ratio = False,
-    add_overflow = True, 
+    add_overflow = False, 
     add_CMSlabel = True,
     add_lumilabel = False,
     CMStag = '"Preliminary"',
@@ -80,7 +185,7 @@ cfg = Config(
     do_counting = False,
     do_shape_analysis = True,
     do_shape_TH1 = False,
-    lumi_target = 41.601,
+    lumi_target = -1,
 
     # if counting, or shape_TH1, choose bkg estimation method
     do_ABCD = False,
@@ -91,17 +196,16 @@ cfg = Config(
     sigma_mult_window = 2,
 
     # if shape analysis
-    signal_model_label = 'voigtian',
+    signal_model_label = 'doubleCB',
     use_discrete_profiling = True,
     ## if not discrete profiling, choose fixed background model
     background_model_label = 'chebychev',
     do_binned_fit = True,
-    do_blind = True,
     mass_window_size = 3,
     fit_window_size = 10,
     nbins = 100, 
     plot_pulls = False,
-    plot_prefit = True,
+    plot_prefit = False,
 
     # further options
     do_categories = True,
@@ -109,14 +213,22 @@ cfg = Config(
     sigma_B = 472.8e9,
     lhe_efficiency = 0.08244,
     do_tdrstyle = False,
-    resolution_p0 = 0.0002747, 
-    resolution_p1 = 0.008302, 
+    resolution_p0 = 6.98338e-04, 
+    resolution_p1 = 7.78382e-03, 
 
 
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
     ###     Limits info     ###
     #-#-#-#-#-#-#-#-#-#-#-#-#-#
-    run_blind = True,
+    do_blind = False,
+
+
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#
+    ### Interpretation info ###
+    #-#-#-#-#-#-#-#-#-#-#-#-#-#
+    muon_eoslabel = '23_04_08_unblinding_Bc_fullscan_nobernstein_v2',
+    electron_eoslabel = '07_04_23_fullStat',
+    coupling_scenarios_label = 'triangle',
 )
 
 
