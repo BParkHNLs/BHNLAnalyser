31221385e06826b4c82517105536d93f8e1c652d
anlyon <alyon@phys.ethz.ch>
2024-06-10 23:04:12 +0200

    script to produce thesis preselection plots

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -189,7 +189,10 @@ class Plotter(Tools):
         legend = self.tools.getRootTLegend(xmin=0.47, ymin=0.65, xmax=0.84, ymax=0.83, size=0.027)
       else:
         #legend = self.tools.getRootTLegend(xmin=0.42, ymin=0.57, xmax=0.79, ymax=0.79, size=0.038)
-        legend = self.tools.getRootTLegend(xmin=0.33, ymin=0.61, xmax=0.59, ymax=0.82, size=0.038)
+        if not do_shape:
+          legend = self.tools.getRootTLegend(xmin=0.33, ymin=0.61, xmax=0.59, ymax=0.82, size=0.038)
+        else:
+          legend = self.tools.getRootTLegend(xmin=0.4, ymin=0.61, xmax=0.66, ymax=0.82, size=0.038)
         #legend = self.tools.getRootTLegend(xmin=0.13, ymin=0.2, xmax=0.45, ymax=0.5, size=0.038)
 
     pad_up.cd()
@@ -408,8 +411,10 @@ class Plotter(Tools):
     elif plot_qcd and plot_sig: frame.GetYaxis().SetRangeUser(1e-4, self.getMaxRangeY(signal_hists, hist_qcd_stack, do_log, use_sig=True))
     #elif plot_data and plot_sig: frame.GetYaxis().SetRangeUser(1e-9, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
     elif plot_data and plot_sig and not do_tdrstyle: frame.GetYaxis().SetRangeUser(1e-4, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
-    elif plot_data and plot_sig and do_tdrstyle and not do_log: frame.GetYaxis().SetRangeUser(1e-4, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True)+0.7*self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
-    elif plot_data and plot_sig and do_tdrstyle and do_log: frame.GetYaxis().SetRangeUser(1000, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True)+1000*self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
+    elif plot_data and plot_sig and do_tdrstyle and not do_log and not do_shape: frame.GetYaxis().SetRangeUser(1e-4, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True)+0.7*self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
+    elif plot_data and plot_sig and do_tdrstyle and not do_log and do_shape: frame.GetYaxis().SetRangeUser(1e-4, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True)+0.2*self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
+    elif plot_data and plot_sig and do_tdrstyle and do_log and not do_shape: frame.GetYaxis().SetRangeUser(1000, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True)+1000*self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
+    elif plot_data and plot_sig and do_tdrstyle and do_log and do_shape: frame.GetYaxis().SetRangeUser(1e-4, self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True)+10*self.getMaxRangeY(signal_hists, hist_data_tot, do_log, use_sig=True))
     #elif plot_data and plot_sig: frame.GetYaxis().SetRangeUser(1e2, 1e7)
 
     #ROOT.gStyle.SetPadLeftMargin(0.16) 
@@ -477,13 +482,14 @@ class Plotter(Tools):
     #else:
     #  self.tools.printLatexBox(0.60, 0.84, title, size=0.04 if plot_ratio else 0.038)
 
-    if do_tdrstyle: self.tools.printLatexBox(0.36, 0.85, 'Dimuon channel', size=0.038, pos='left', font=42)
-    if do_tdrstyle: self.tools.printLatexBox(0.405, 0.575, '(#it{r}_{e}, #it{r}_{#mu}, #it{r}_{#tau}) = (0, 1, 0), Majorana', size=0.038, pos='left', font=42)
+    if do_tdrstyle and not do_shape: self.tools.printLatexBox(0.36, 0.85, 'Dimuon channel', size=0.038, pos='left', font=42)
+    if do_tdrstyle and do_shape: self.tools.printLatexBox(0.43, 0.85, 'Dimuon channel', size=0.038, pos='left', font=42)
+    if not do_shape and do_tdrstyle: self.tools.printLatexBox(0.405, 0.575, '(#it{r}_{e}, #it{r}_{#mu}, #it{r}_{#tau}) = (0, 1, 0), Majorana', size=0.038, pos='left', font=42)
     #self.tools.printLatexBox(0.38, 0.535, '(#it{r}_{e}, #it{r}_{#mu}, #it{r}_{#tau}) = (0, 1, 0), Majorana', size=0.041, pos='left', font=42)
     #legend = self.tools.getRootTLegend(xmin=0.4, ymin=0.63, xmax=0.75, ymax=0.86, size=0.038)
     if add_CMSlabel and not do_tdrstyle: self.tools.printCMSTag(pad_up, CMS_tag, size=0.55 if plot_ratio else 0.43)
     if add_CMSlabel and do_tdrstyle: self.tools.printInnerCMSTag(pad_up, CMS_tag, True, x_pos=0.17, y_pos=0.835, size=0.55)
-    self.tools.printLumiTag(pad_up, 41.6, size=0.5, offset=0.507)
+    if not do_shape: self.tools.printLumiTag(pad_up, 41.6, size=0.5, offset=0.507)
 
     #if do_luminorm:
     #  scale_text = ROOT.TPaveText(0.15, 0.83, 0.3, 0.88, "brNDC")
